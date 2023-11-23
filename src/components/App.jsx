93e920cb2f04438034e569e1f10dc953161301b5
NatalyBMota <138526575+NatalyBMota@users.noreply.github.com>
2023-11-23 17:24:53 -0600

    Removed one more unecessary comment.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './Header.jsx';
 import Navbar from './Navbar.jsx';
-//import Form from './Form.jsx';
 import Main from './Main.jsx';
 import CardWrapper from './CardWrapper.jsx';
 import Footer from './Footer.jsx';
