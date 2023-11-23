87cd6573272484240510f5ec7a761e4bf54248ba
NatalyBMota <138526575+NatalyBMota@users.noreply.github.com>
2023-11-23 17:23:10 -0600

    Divided the Header component into two components, by creating a new Message component that is being used in the Header component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Message from './Message';
 //import holidayStyle from './holidayStyle';
 
 const headerStyle = {
@@ -6,23 +7,10 @@ const headerStyle = {
 	backgroundColor: 'rgba(212,175,55, .5)',
 }
 
-const userBirthYear = 1996;
-//const userInput = new Array(1996, 1190);
-const currentTime = new Date();
-const currentYear = currentTime.getFullYear();
-let msg;
-
-if ((currentYear !== userBirthYear) && (userBirthYear >  (currentYear - 16))) {
-	msg = 'CONGRATS! you can apply for driver license! 😄';
-} else {
-	msg = 'Sorry! You can NOT apply for a driver license! 🙁';
-}
-
-
 // this is a component
 function Header() {
 	return <header style={headerStyle}>
-	  <h1 className="header">{msg}</h1>
+	  <h1 className="header"><Message /></h1>
 	</header>;
 }
 
---
+++ src/components/Message.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const userBirthYear = 1996;
+//const userInput = new Array(1996, 1190);
+const currentTime = new Date();
+const currentYear = currentTime.getFullYear();
+let msg;
+
+if ((currentYear !== userBirthYear) && (userBirthYear >  (currentYear - 16))) {
+	msg = 'CONGRATS! you can apply for driver license! 😄';
+} else {
+	msg = 'Sorry! You can NOT apply for a driver license! 🙁';
+}
+
+function Message() {
+	return <span>{msg}</span>
+}
+
+export default Message;
